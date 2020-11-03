af898e102f26cebf70e337f39a20cbf01fda0eef
bentoavb <bentoavb@gmail.com>
2020-11-03 11:38:33 -0300

    alterando estrutura

--- script/index.js
+++ script/index.js
@@ -1,20 +1,22 @@
-let t = 0;
-let T = 10; 
-let T_nuvem = 50; 
-let t_vel = 10;
+let t = 0; // Variável de tempo [segundos]
+let T = 10; // Tempo de amostragem [milisegundos]
+let T_nuvem = 50; // Tempo de amostragem das nuvens [milisegundos]
+let t_vel = 10; // Multiplicador para acelerar a simulação
 
-let vel0 = 100;
-let alpha = 45;
-let y0 = 0;
-let beta = 0;
+let vel0 = 100; // Velocidade inicial do projétil [metros por segundos]
+let alpha = 45; // Ângulo de lançamento [graus]
+let y0 = 0; // Posição vertical inicial [metros]
+let beta = 0; // coeficiente da resistência do ar
 
 let zoom = 1;
 
-let lancamentoArray = [];
+let lancamentoArray = []; // Array para armazenar informações dos lançamentos
 
-w = window.innerWidth;
-h = window.innerHeight*0.8;
+w = window.innerWidth; // Tamanho horizontal do Canvas
+h = window.innerHeight*0.8; // Tamanho vertical do Canvas
 
+// Função executada sempre que a tela mudar de tamanho
+// Quando isso acontece, é necessário reconfigurar o Canvas de cada camada da aplicação
 window.addEventListener("resize", function () {
     w = window.innerWidth;
     h = window.innerHeight*0.8;
@@ -32,5 +34,3 @@ window.addEventListener("resize", function () {
     redesenharLayer3();
 
 });
-
-document.fonts.ready.then(_ => console.log('...'));
